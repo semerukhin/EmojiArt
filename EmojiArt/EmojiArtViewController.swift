188014aba98ec07ebd27541b2be643f417612206
Ilya Semerukhin <semerukhin@gmail.com>
2018-08-19 16:43:19 +0300

    Add the bar with the collection of emojis

--- EmojiArt/EmojiArtViewController.swift
+++ EmojiArt/EmojiArtViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScrollViewDelegate {
+class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScrollViewDelegate, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
    
    @IBOutlet weak var dropZone: UIView! {
       didSet {
@@ -54,6 +54,32 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScr
             scrollView?.zoomScale = max(dropZone.bounds.size.width / size.width, dropZone.bounds.size.height / size.height)
          }
       }
+   }
+   
+   var emojis = "🌝🐝🐓🐇🐞🐌🐛🍏🍊🍇🥔✨".map { String($0) }
+   
+   @IBOutlet weak var emojiCollectionView: UICollectionView! {
+      didSet {
+         emojiCollectionView.dataSource = self
+         emojiCollectionView.delegate = self
+      }
+   }
+   
+   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+      return emojis.count
+   }
+   
+   private var font: UIFont {
+      return UIFontMetrics(forTextStyle: .body).scaledFont(for: UIFont.preferredFont(forTextStyle: .body).withSize(60.0))
+   }
+   
+   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+      let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "EmojiCell", for: indexPath)
+      if let emojiCell = cell as? EmojiCollectionViewCell {
+         let text = NSAttributedString(string: emojis[indexPath.item], attributes: [.font: font])
+         emojiCell.label.attributedText = text
+      }
+      return cell
    }
    
    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
---
+++ EmojiArt/EmojiCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by Ilya Semerukhin on 19.08.2018.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+   
+   @IBOutlet weak var label: UILabel!
+   
+}
+
+
+
+
+
