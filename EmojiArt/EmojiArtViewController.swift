bf295b3e34830edab0377c1dffb31da0d948d311
Ilya Semerukhin <semerukhin@gmail.com>
2018-08-19 11:44:14 +0300

    Make an image scrollable

--- EmojiArt/EmojiArtViewController.swift
+++ EmojiArt/EmojiArtViewController.swift
@@ -7,11 +7,42 @@
 
 import UIKit
 
-class EmojiArtViewController: UIViewController, UIDropInteractionDelegate {
+class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScrollViewDelegate {
    
    @IBOutlet weak var dropZone: UIView! {
       didSet {
          dropZone.addInteraction(UIDropInteraction(delegate: self))
+      }
+   }
+   
+   var emojiArtView = EmojiArtView()
+   
+   @IBOutlet weak var scrollView: UIScrollView! {
+      didSet {
+         scrollView.minimumZoomScale = 0.1
+         scrollView.maximumZoomScale = 5.0
+         scrollView.delegate = self
+         scrollView.addSubview(emojiArtView)
+      }
+   }
+   
+   func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+      return emojiArtView
+   }
+   
+   var emojiArtBackgroundImage: UIImage? {
+      get {
+         return emojiArtView.backgroundImage
+      }
+      set {
+         scrollView?.zoomScale = 1.0
+         emojiArtView.backgroundImage = newValue
+         let size = newValue?.size ?? CGSize.zero
+         emojiArtView.frame = CGRect(origin: CGPoint.zero, size: size)
+         scrollView?.contentSize = size
+         if let dropZone = self.dropZone, size.width > 0, size.height > 0 {
+            scrollView?.zoomScale = max(dropZone.bounds.size.width / size.width, dropZone.bounds.size.height / size.height)
+         }
       }
    }
    
@@ -28,7 +59,7 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate {
    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
       imageFetcher = ImageFetcher() { (url, image) in
          DispatchQueue.main.async {
-            self.emojiArtView.backgroundImage = image
+            self.emojiArtBackgroundImage = image
          }
       }
       session.loadObjects(ofClass: NSURL.self) { nsurls in
@@ -42,8 +73,6 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate {
          }
       }
    }
-   
-   @IBOutlet weak var emojiArtView: EmojiArtView!
    
 }
 
